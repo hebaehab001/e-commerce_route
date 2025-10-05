cddc8e6ce37267bcca4f6446b2ca80a4cd2f4767
hebaehab2001 <hebaehab74@gmail.com>
2025-10-05 20:58:47 +0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,20 @@ import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 const nextConfig = {
      images: {
         domains: ['ecommerce.routemisr.com']
-    }
+    },
+    async rewrites() {
+        return [
+            {
+                // 1. Local path your client code will use
+                source: '/api/auth/v1/:path*',
+
+                // 2. The external destination (the actual API server)
+                // Next.js server makes this request, bypassing CORS
+                destination: 'https://ecommerce.routemisr.com/api/v1/:path*',
+            },
+        ];
+    },
+
 };
 
 export default withFlowbiteReact(nextConfig);
